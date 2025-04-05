1bf5405b12447cb6ccd22fdba1c8fd9f865b88c3
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2025-04-05 22:07:01 +0700

    end

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
   process.env.DB_DATABASE || "railway",
   process.env.DB_USERNAME || "root",
-  process.env.DB_PASSWORD || "HqEcUecQHKeIVoPKURDxKfpxCCPYllwL",
+  process.env.DB_PASSWORD || "auAdrAVmFkquAMKLJkhDVisPqgkDvJPP",
   {
-    host: process.env.DB_HOST || "mainline.proxy.rlwy.net",
+    host: process.env.DB_HOST || "shuttle.proxy.rlwy.net",
     dialect: process.env.DB_DIALECT || 'mysql',
-    port: process.env.DB_PORT || 17820,
+    port: process.env.DB_PORT || 52639,
     logging: false,
     dialectOptions: {
       connectTimeout: 20000,
