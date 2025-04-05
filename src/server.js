2b9a1bc72b42c39a5b18e0ccce18e34c642078b9
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2025-04-05 14:11:04 +0700

    ok cc

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(cors({
   origin: ['https://nguyentrgkien.github.io/mange-restaurant-ui', 'https://nguyentrgkien.github.io'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization',  'ngrok-skip-browser-warning']
 }));
 initRoute(app);
 
