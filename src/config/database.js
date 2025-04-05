bc40bf10ecfa5fcb6cfa3cf7fc5e818435481cf9
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2025-04-05 20:14:47 +0700

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,20 @@
 import { Sequelize } from "sequelize";
 
-// Khởi tạo Sequelize với biến môi trường
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,          // Tên database
-  process.env.MYSQLUSER,             // Tên người dùng
-  process.env.MYSQLPASSWORD, // Mật khẩu
+  process.env.DB_DATABASE || "railway",
+  process.env.DB_USERNAME || "root",
+  process.env.DB_PASSWORD || "HqEcUecQHKeIVoPKURDxKfpxCCPYllwL",
   {
-    host: process.env.MYSQLHOST, // Host
-    dialect: "mysql",                               // Loại database
-    port: process.env.MYSQLPORT || 17820,             // Port
-    logging: false,                                 // Tắt log SQL
+    host: process.env.DB_HOST || "mainline.proxy.rlwy.net",
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.DB_PORT || 17820,
+    logging: false,
     dialectOptions: {
-      connectTimeout: 20000,                       // Timeout 20 giây
+      connectTimeout: 20000,
     },
   }
 );
 
-// Hàm kết nối cơ sở dữ liệu
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
